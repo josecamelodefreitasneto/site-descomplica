f8a9a26a956224c9501d8a0007c1a9e7f9fa2d0e
josecamelo <camelogamerdf@gmail.com>
2024-03-31 16:12:38 -0300

    estudo do if

---
+++ if.js
@@ -0,0 +1,18 @@
+var pais = "peru";
+
+if(pais != "Brasil"){
+    console.log("Voce é estrangeiro");
+}else {
+    "Voçê é Brasileiro" 
+}
+
+
+var idade = 66;
+if(idade < 16){
+    console.log("Votar não tem idade para votar ");
+
+}else if(idade < 18 || idade > 65) {
+    console.log("Voce não é obrigado a votar")
+}else{
+    console.log("Voto obrigatório")
+}
